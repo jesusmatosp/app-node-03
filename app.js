153b20f0e5797681d155e32062bff9c460e8da89
Jesús M. Portocarrero <jesus.matosp@gmail.com>
2019-12-19 18:48:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! - demo");
+  res.send("Hello from Appsody! - demo 22222");
 });
  
 module.exports.app = app;
